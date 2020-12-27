19086839e024e1650a859861bd5fc8ef12d9d26a
saulcp97 <saulcp97@gmail.com>
2020-12-27 15:42:58 +0100

    Get IP Socket (gIP)

--- Master.js
+++ Master.js
@@ -1,17 +1,24 @@
 // Master.js
 var zmq = require("zeromq"),
-	mDictionary = zmq.socket("router");
-
+	mDictionary = zmq.socket("rep");
 //Direction Dictionary
-var dDictionary = {}
+var dDictionary = {
+	["LBF"]: "tcp://127.0.0.1:3010",
+	["LBQ"]: "tcp://127.0.0.1:3020",
+	["QUEUE"]: "tcp://127.0.0.1:3030"
+};
 
 //We could initiate
-
-mDictionary.bind("tcp://127.0.0.1:3030");
+mDictionary.bind("tcp://127.0.0.1:3000");
 console.log("MASTER Start");
 
-
-mDictionary.on("message", function(chain) {
-	console.log("Keep Alive");
-	console.log(chain)
+mDictionary.on("message", function(obj) {
+	console.log(`Received Request: ${obj}`);
+    // Send the message back as a reply to the server.
+	const message = dDictionary[obj.toString("utf8")];
+	if (message == null) {
+		mDictionary.send("NaN");
+	} else {	
+		mDictionary.send(message);
+	}
 });
